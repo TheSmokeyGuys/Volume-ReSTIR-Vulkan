5fb54d82fcc8f1641da9e45b631e9d2ecc78c543
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-11-14 01:27:25 -0500

    add vk_bootstrap; add sample main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
+#include "VkBootstrap.h"
 #include "spdlog/spdlog.h"
 
 int main(int argc, char const *argv[]) {
   spdlog::info("Hello from Volumetric-ReSTIR project!");
+
+  vkb::InstanceBuilder instance_builder;
+  auto instance_build_success = instance_builder.set_app_name("Volume ReSTIR")
+                                    .request_validation_layers()
+                                    .use_default_debug_messenger()
+                                    .build();
+  if (!instance_build_success) {
+    spdlog::error("Failed to create Vulkan instance: {}",
+                  instance_build_success.error().message());
+  }
   return 0;
 }
