19e270602092948e91ad174232d5c316369a5862
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-11-15 18:49:08 -0500

    add window class

---
+++ src/Window.hpp
@@ -0,0 +1,37 @@
+#ifndef __VOLUME_RESTIR_WINDOW_HPP__
+#define __VOLUME_RESTIR_WINDOW_HPP__
+
+#ifdef _WIN32
+#pragma comment(linker, "/subsystem:console")
+#include <windows.h>
+#elif defined(__linux__)
+#include <xcb/xcb.h>
+#endif
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <string>
+
+namespace volume_restir {
+
+class Window {
+public:
+  Window();
+  ~Window();
+
+  bool ShouldQuit() const { return !!glfwWindowShouldClose(window_); }
+  int Width() const noexcept { return width_; }
+  int Height() const noexcept { return height_; }
+  std::string Name() const noexcept { return name_; }
+
+private:
+  GLFWwindow* window_;
+  int width_;
+  int height_;
+  std::string name_;
+};
+
+}  // namespace volume_restir
+
+#endif /* __VOLUME_RESTIR_WINDOW_HPP__ */
