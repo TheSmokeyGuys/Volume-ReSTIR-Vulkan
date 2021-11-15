111938c3013b7f4a5b89a3ad8e427a0615eaea33
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-11-15 18:49:22 -0500

    add static config files

---
+++ src/static_config.cpp
@@ -0,0 +1,11 @@
+#include "static_config.hpp"
+
+namespace volume_restir {
+namespace static_config {
+
+const std::string kApplicationName = "Volume ReSTIR";
+const int kWindowWidth             = 1920;
+const int kWindowHeight            = 1080;
+
+}  // namespace static_config
+}  // namespace volume_restir
---
+++ src/static_config.hpp
@@ -0,0 +1,16 @@
+#ifndef __VOLUME_RESTIR_STATIC_CONFIG_HPP__
+#define __VOLUME_RESTIR_STATIC_CONFIG_HPP__
+
+#include <string>
+
+namespace volume_restir {
+namespace static_config {
+
+extern const std::string kApplicationName;
+extern const int kWindowWidth;
+extern const int kWindowHeight;
+
+}  // namespace static_config
+}  // namespace volume_restir
+
+#endif /* __VOLUME_RESTIR_STATIC_CONFIG_HPP__ */
