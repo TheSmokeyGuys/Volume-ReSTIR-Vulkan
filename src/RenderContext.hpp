2c7e158901f5325478821a6299b8f5bbd7f25aca
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-11-16 20:03:47 -0500

    add documentation to render context

--- src/RenderContext.hpp
+++ src/RenderContext.hpp
@@ -1,6 +1,17 @@
 #ifndef __VOLUME_RESTIR_INSTANCE_HPP__
 #define __VOLUME_RESTIR_INSTANCE_HPP__
 
+/**
+ * @file RenderContext.hpp
+ * @author Zhihao Ruan (ruanzh@seas.upenn.edu)
+ *
+ * @brief RenderContext should be a singleton that holds all the shared global
+ * rendering contexts for this application. Please checkout SingletonManager.hpp
+ * for how to use singletons.
+ *
+ * @date 2021-11-16
+ */
+
 #include <vulkan/vulkan.h>
 
 #include "VkBootstrap.h"
