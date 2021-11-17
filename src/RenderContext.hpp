592ab0b0665ffeaa550638ff908b740e8ecd8b0a
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-11-17 05:39:45 -0500

    add getters to member variables

--- src/RenderContext.hpp
+++ src/RenderContext.hpp
@@ -21,6 +21,16 @@ public:
   RenderContext();
   ~RenderContext();
 
+  vkb::Instance& Instance() { return instance_; }
+  VkSurfaceKHR Surface() { return surface_; }
+  vkb::Device& Device() { return device_; }
+  vkb::Swapchain& Swapchain() { return swapchain_; }
+  const vkb::Instance& Instance() const { return instance_; }
+  const vkb::Device& Device() const { return device_; }
+  const vkb::Swapchain& Swapchain() const { return swapchain_; }
+
+  void CreateSwapChain();
+
 private:
   vkb::Instance instance_;
   VkSurfaceKHR surface_;
