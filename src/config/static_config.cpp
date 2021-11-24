da462886d8a3fd5701fdcdd858b314c97b3a2f9c
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-11-23 20:26:21 -0500

    add static config for field of view

--- src/config/static_config.cpp
+++ src/config/static_config.cpp
@@ -7,6 +7,7 @@ const std::string kApplicationName = "Volume ReSTIR";
 const int kWindowWidth             = 1920;
 const int kWindowHeight            = 1080;
 const int kMaxFrameInFlight        = 2;
+const float kFOVInDegrees          = 45.0f;
 
 }  // namespace static_config
 }  // namespace volume_restir
